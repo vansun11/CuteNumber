b2e400d12ef77f1b9d2a06412df327a61c60480d
Qiu Wan <QWan@slb.com>
2022-10-25 17:22:00 +0800

    feat: add cuteNumber test

--- src/Foo.ts
+++ src/Foo.ts
@@ -3,3 +3,26 @@ export class Foo {
     return 'bar'
   }
 }
+
+export function isCuteNumberGreaterThan10(numbers: number[]) {
+
+  const evenNumbers: number[] = findAllEvenNumbers(numbers)
+  // console.log(evenNumbers)
+  const maxNumber: number = findTheMax(evenNumbers)
+  console.log(maxNumber)
+  return isGreaterThan10(maxNumber)
+
+}
+
+function findAllEvenNumbers(numbers: number[]) {
+  return numbers.filter(number => number % 2 === 0)
+}
+
+function findTheMax(evenNumbers: number[]): number {
+  return Math.max(...evenNumbers)
+}
+
+function isGreaterThan10(maxNumber: number) {
+  return maxNumber > 10
+}
+
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,18 @@
-import {Foo} from '../src/Foo'
+import { Foo } from '../src/Foo'
+import { isCuteNumberGreaterThan10 } from '../src/Foo'
 
 describe('Foo', () => {
   it('bar', () => {
     expect(new Foo().bar()).toEqual('bar')
   })
+
+  it('should return true when given special numbers', () => {
+    const numbers = [1, 3, 4, 5, 7, 8, 20]
+
+    const result: boolean = isCuteNumberGreaterThan10(numbers)
+
+    expect(result).toBeTruthy()
+    // expect(result).toBeFalsy()
+  })
+
 })
